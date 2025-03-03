ce64fe27698b0c2e1737961ba96a45bd0ee61ac4
Lauri Makkonen <ag4039@student.jamk.fi>
2025-03-03 09:19:49 +0200

    got a basic calendar view to work

---
+++ gamify.js
@@ -0,0 +1,7 @@
+let field = document.getElementById('datepicker');
+let picker = new Pikaday({
+  onSelect: function (date) {
+    field.value = picker.toString();
+  },
+});
+field.parentNode.insertBefore(picker.el, field.nextSibling);
