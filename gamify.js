f4cde9ce1002b144184017aa64f04d00544c98f9
Lauri Makkonen <ag4039@student.jamk.fi>
2025-03-06 09:13:11 +0200

    made calendar to append itself inside container

--- gamify.js
+++ gamify.js
@@ -1,11 +1,14 @@
-let field = document.getElementById('datepicker-calendar');
-let picker = new Pikaday({
-  onSelect: function (date) {
-    field.value = picker.toString();
-  },
+// variable for container of calendar
+const calendarContainer =
+  document.getElementsByClassName('calendar-container')[0];
+
+// variable for calendar itself
+const calendar = new Pikaday({
   firstDay: 1,
   showWeekNumber: true,
+  onSelect: function (date) {
+    // field.value = picker.toString();
+    // this.hide();
+  },
 });
-field.parentNode.insertBefore(picker.el, field.nextSibling);
-
-let selectedDate = picker.getDate();
+calendarContainer.appendChild(calendar.el);
